ceaec43a85647a4381f8f3c0fded4590aec2f81b
Sahul54 <ksahul9576@gmail.com>
2024-06-27 14:29:36 +0530

    log page some issues

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home"
+// import OpenRoute from "./components/core/Auth/OpenRoute";
+// import Login from "./pages/Login";
+// import Signup from './pages/Signup';
 
 
 function App() {
   return (
     <div className='w-screen min-h-screen bg-richblack-900 flex-col font-inter'>
      <Routes>
+
       <Route path= "/" element={<Home/>} />
+      {/* <Route 
+        path="signup"
+        element= {
+                  <OpenRoute>
+                    <Signup/>
+                  </OpenRoute>
+        }
+        />
+
+        <Route
+          path="login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+          /> */}
+          
      </Routes>
     </div>
   );
